809137b70ae1ce3cc1499568bb98823b04ddf2eb
Boen Andersen <boendandersen@gmail.com>
2019-06-18 11:06:54 -0500

    lots of blog stuff only

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+
+import com.example.demo.model.Post;
+import com.example.demo.model.User;
+
+public interface PostService {
+
+    Optional<Post> findForId(Long id);
+
+    Post save(Post post);
+
+    /**
+     * Finds a {@link Page) of {@link Post} of provided user ordered by date
+     */
+    Page<Post> findByUserOrderedByDatePageable(User user, int page);
+
+    /**
+     * Finds a {@link Page) of all {@link Post} ordered by date
+     */
+    Page<Post> findAllOrderedByDatePageable(int page);
+
+    void delete(Post post);
+}
+
