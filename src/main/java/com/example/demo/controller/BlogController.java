874808946158cd309c0bcabc26f010e34ee3b6fb
Nick <nick.helman@gmail.com>
2019-06-24 11:01:30 -0400

    hopefully added all of boen's work.

---
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Post;
+import com.example.demo.model.User;
+import com.example.demo.service.PostService;
+import com.example.demo.service.UserService;
+import com.example.demo.util.Pager;
+
+public class BlogController {
+
+    private final UserService userService;
+
+    private final PostService postService;
+
+    @Autowired
+    public BlogController(UserService userService, PostService postService) {
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/blog/{username}", method = RequestMethod.GET)
+    public String blogForUsername(@PathVariable String username,
+                                  @RequestParam(defaultValue = "0") int page,
+                                  Model model) {
+
+        Optional<User> optionalUser = userService.findByUsername(username);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            Page<Post> posts = postService.findByUserOrderedByDatePageable(user, page);
+            Pager pager = new Pager(posts);
+
+            model.addAttribute("pager", pager);
+            model.addAttribute("user", user);
+
+            return "/posts";
+
+        } else {
+            return "/error";
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/myPosts/MyPosts.java
@@ -0,0 +1,38 @@
+package com.example.demo.myPosts;
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
+public class MyPosts {
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
+	    private String name;
+	    private String content;
+
+	    public Integer getId() {
+	        return id;
+	    }
+
+	    public void setId(Integer id) {
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getContent() {
+	        return content;
+	    }
+
+	    public void setContent(String content) {
+	        this.content = content;
+	    }
+	}
+	
+}
