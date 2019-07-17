4676db3516ce1f8c8d32350c581eb3c315ef7202
Nick <nick.helman@gmail.com>
2019-07-17 09:20:06 -0400

    working login and signup backend

---
+++ src/main/java/who/banh/lsp/WhoApp.java
@@ -0,0 +1,13 @@
+package who.banh.lsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoApp.class, args);
+	}
+
+}
